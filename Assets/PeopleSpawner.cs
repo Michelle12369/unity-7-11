0a92d1f0bc2b2342ad58e5686a9ec03bd3aa1023
michelle12369 <p5i7gaaaa@gmail.com>
2016-12-24 16:12:54 +0800

    people walking

---
+++ Assets/PeopleSpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class PeopleSpawner : MonoBehaviour {
+
+	public GameObject PeopleCandidate;
+	public List<Transform> SpawnPoint;
+	public float SpawnTime = 10;
+	private float SpawnCounter = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SpawnCounter += Time.deltaTime;
+		if (SpawnCounter >= SpawnTime) {
+			SpawnCounter = 0;
+			GameObject newPeople = GameObject.Instantiate (PeopleCandidate);
+		}
+	}
+}
---
+++ Assets/Script/PeopleController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PeopleController : MonoBehaviour {
+
+	private Animator animatorController;
+	public GameObject item;//get item
+	public GameObject dollar;//get dollar
+	private Renderer rend;//get item mesh renderer
+	private TextMesh text;
+	// Use this for initialization
+	void Start () {
+		animatorController = this.GetComponent<Animator> ();
+		rend = item.GetComponent<Renderer>();
+		Color newColor =  new Color( Random.value, Random.value, Random.value, 1.0f );
+		rend.material.color = newColor;
+		text=dollar.GetComponent<TextMesh> ();
+		text.text = "$"+Random.Range(0, 200).ToString();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(this.transform.position.x < -0.5f){
+		animatorController.SetBool ("walking", true);
+		this.transform.Translate(Vector3.forward * Time.deltaTime);
+		}else if (this.transform.position.x > -0.5f){
+			animatorController.SetBool ("walking", false);
+			transform.rotation = Quaternion.AngleAxis(145, Vector3.up);
+		}
+
+	}
+	void OnTriggerEnter(Collider coll) {//撞到
+		Debug.Log ("Collision" + coll.gameObject.transform.position.x);
+
+		//this.transform.Translate(Vector3.forward*0); 
+//				this.transform.position.x= coll.gameObject.transform.position.x;
+	}
+}
